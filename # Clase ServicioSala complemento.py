2406aa3bf952eb736b373b2add0942125b0ce847
cabezasmarli1003-web <cabezasmarli10.03@gmail.com>
2026-05-09 15:00:59 -0500

    tercer comit,complemento de la clase abstracta, implementando el poliformismo y metodos sobre escrito

---
+++ # Clase ServicioSala complemento.py
@@ -0,0 +1,24 @@
+
+# Clase ServicioSala 
+class ServicioSala(Servicio):
+
+    # Calcula costo por horas con recargo del 10%
+    def calcular_costo(self, horas=1):
+        return self.precio_base * horas * 1.1
+
+
+# Clase ServicioEquipo 
+class ServicioEquipo(Servicio):
+
+    # Calcula costo por días con descuento del 5%
+    def calcular_costo(self, dias=1):
+        return self.precio_base * dias * 0.95
+
+
+# Clase ServicioAsesoria
+class ServicioAsesoria(Servicio):
+
+    # Calcula costo por horas con recargo del 20%
+    def calcular_costo(self, horas=1):
+        return self.precio_base * horas * 1.2
+    # Mrly C
